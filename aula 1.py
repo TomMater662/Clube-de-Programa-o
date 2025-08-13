5b43fb9142c8f9c17886c910dc6528c325d58031
TomMater662 <breno.rodarte@loyola.g12.br>
2025-08-13 14:54:04 -0300

    Create aula 1.py

---
+++ aula 1.py
@@ -0,0 +1,8 @@
+nun7 = int(input('Digite a primeira nota'))
+nun8 = int(input('Digite a segunda nota'))
+nun9 = int(input('Digite a terceira nota'))
+nun10 = int(input('Digite a quarta nota'))
+resultado = 0
+
+resultado = (nun7 + nun9 + nun8 + nun10) / 4 
+print('a média é resultado' +str(resultado))
